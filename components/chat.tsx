1188529cb052f7ec780b114e06f340b0ef60cbdf
Brayan Muniz <35124364+brayanMuniz@users.noreply.github.com>
2023-07-22 09:18:01 -0500

    Adds furigana to only kanji

--- components/chat.tsx
+++ components/chat.tsx
@@ -68,6 +68,13 @@ export function Chat({}: ChatProps) {
   }
 
   // Component to render text with ruby annotations
+  const isKanji = (ch: string) => {
+    return (
+      (ch >= "\u4e00" && ch <= "\u9faf") || // CJK Unified Ideographs
+      (ch >= "\u3400" && ch <= "\u4dbf") // CJK Unified Ideographs Extension A
+    );
+  };
+
   const RubyText: React.FC<RubyTextProps> = ({ text, tokenizer }) => {
     if (!tokenizer) return <>{text}</>;
 
@@ -75,7 +82,7 @@ export function Chat({}: ChatProps) {
     return (
       <>
         {tokens.map((token, index) =>
-          token.reading ? (
+          token.reading && [...token.surface_form].some(isKanji) ? (
             <ruby key={index}>
               {token.surface_form}
               <rt>{token.reading}</rt>
