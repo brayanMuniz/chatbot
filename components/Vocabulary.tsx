a3b555fd04485965dae1813ee0caf41046ccd6c7
Brayan Muniz <35124364+brayanMuniz@users.noreply.github.com>
2023-07-28 08:52:40 -0500

    Getting Wanikani Data

--- components/Vocabulary.tsx
+++ components/Vocabulary.tsx
@@ -1,18 +1,43 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 interface VocabularyProps {
   kanjiList: string[];
 }
 
 const Vocabulary: React.FC<VocabularyProps> = ({ kanjiList }) => {
+  const [kanjiData, setKanjiData] = useState<any[]>([]);
+
+  useEffect(() => {}, []);
+
+  useEffect(() => {
+    const fetchKanjiData = async () => {
+      // ! I am getting ALL the kanji
+      // const newData = await Promise.all(
+      //   kanjiList.map(async (kanji) => {
+      //     const response = await axios.get(
+      //       `https://api.wanikani.com/v2/subjects?types=kanji&characters=${kanji}`,
+      //       {
+      //         headers: {
+      //           Authorization: `Bearer ${process.env.NEXT_PUBLIC_WANIKANI_KEY}`,
+      //         },
+      //       }
+      //     );
+      //     return response.data.data[0].data;
+      //   })
+      // );
+      // setKanjiData(newData);
+    };
+
+    fetchKanjiData();
+  }, [kanjiList]);
+
   return (
     <div className="w-3/12">
       <h2>Vocabulary List</h2>
-      <ul>
-        {kanjiList.map((kanji, index) => (
-          <li key={index}>{kanji}</li>
-        ))}
-      </ul>
+      <ul></ul>
     </div>
   );
 };
